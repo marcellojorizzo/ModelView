ffe32889c4db6d50a9c4874069c6696d1496270f
marcellojorizzo <marcello.jorizzo@gmx.net>
2024-09-20 21:55:02 +0200

    fixes added

--- dialog.cpp
+++ dialog.cpp
@@ -1,5 +1,3 @@
-//dialog.cpp
-
 #include "dialog.h"
 #include "ui_dialog.h"
 
@@ -28,4 +26,57 @@ Dialog::Dialog(QWidget *parent)
     ui->comboBox->setModel(model);
 
     // Enable editing in the listView by pressing any key or double-clicking an item
-    ui->listView->setEditTriggers(QAbstractItemView::AnyKeyPressed | QAbstractItemView::
+    ui->listView->setEditTriggers(QAbstractItemView::AnyKeyPressed | QAbstractItemView::DoubleClicked);
+}
+
+// Destructor for the Dialog class
+Dialog::~Dialog()
+{
+    // Clean up and delete the UI object
+    delete ui;
+}
+
+// Slot for adding a new item when the first button is clicked
+void Dialog::on_pushButton_clicked()
+{
+    // Get the current number of rows in the model
+    int row = model->rowCount();
+
+    // Insert a new empty row at the end of the list
+    model->insertRows(row, 1);
+
+    // Get the index of the new row
+    QModelIndex index = model->index(row);
+
+    // Set the newly added row as the current selection in the listView
+    ui->listView->setCurrentIndex(index);
+
+    // Open the editor to allow editing the new item
+    ui->listView->edit(index);
+}
+
+// Slot for inserting a new item above the currently selected one
+void Dialog::on_pushButton_2_clicked()
+{
+    // Get the index of the currently selected item in the listView
+    int row = ui->listView->currentIndex().row();
+
+    // Insert a new empty row above the currently selected row
+    model->insertRows(row, 1);
+
+    // Get the index of the newly inserted row
+    QModelIndex index = model->index(row);
+
+    // Set the newly inserted row as the current selection in the listView
+    ui->listView->setCurrentIndex(index);
+
+    // Open the editor to allow editing the new item
+    ui->listView->edit(index);
+}
+
+// Slot for deleting the currently selected item when the third button is clicked
+void Dialog::on_pushButton_3_clicked()
+{
+    // Remove the row of the currently selected item from the model
+    model->removeRows(ui->listView->currentIndex().row(), 1);
+}
