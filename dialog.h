95bccfb8a895b624875a761452e4021318497f05
marcellojorizzo <marcello.jorizzo@gmx.net>
2024-09-20 21:41:57 +0200

    comments added

--- dialog.h
+++ dialog.h
@@ -1,34 +1,57 @@
 #ifndef DIALOG_H
 #define DIALOG_H
 
+// Include the QDialog class for creating dialog windows
 #include <QDialog>
+
+// Include core functionality (e.g., data models, event handling)
 #include <QtCore>
+
+// Include graphical components and functionalities (e.g., drawing, rendering)
 #include <QtGui>
 
-
+// Start the Qt namespace block for forward declarations
 QT_BEGIN_NAMESPACE
+
+// Declare the Ui namespace and the Dialog class inside it
 namespace Ui {
 class Dialog;
 }
+
+// End the Qt namespace block
 QT_END_NAMESPACE
 
+// Declare the Dialog class that inherits from QDialog to create a custom dialog window
 class Dialog : public QDialog
 {
+    // Enable signals and slots mechanism in this class
     Q_OBJECT
 
 public:
+    // Constructor: Initializes the dialog with an optional parent widget
     Dialog(QWidget *parent = nullptr);
+
+    // Destructor: Cleans up when the Dialog object is destroyed
     ~Dialog();
 
 private slots:
+    // Slot function triggered when the first button is clicked
     void on_pushButton_clicked();
 
+    // Slot function triggered when the second button is clicked
     void on_pushButton_2_clicked();
 
+    // Slot function triggered when the third button is clicked
     void on_pushButton_3_clicked();
 
 private:
+    // Pointer to the user interface components designed in Qt Designer
     Ui::Dialog *ui;
-    QStringListModel * model;
+
+    // Pointer to a QStringListModel, used to store and manipulate a list of strings
+    QStringListModel *model;
 };
+
+// End the include guard to avoid multiple inclusions of the same header file
 #endif // DIALOG_H
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
+// main.cpp
+
+// Includes the header file for the Dialog class
 #include "dialog.h"
 
+// Includes the QApplication class for the Qt application
 #include <QApplication>
 
+// The main function where the program starts
 int main(int argc, char *argv[])
 {
+    // Creates a QApplication object, which manages application-wide resources
     QApplication a(argc, argv);
+
+    // Creates an instance of the Dialog class (the main window)
     Dialog w;
+
+    // Shows the main window (Dialog)
     w.show();
+
+    // Enters the application's event loop and waits for user interactions
     return a.exec();
 }
